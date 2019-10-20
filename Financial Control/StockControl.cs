68204d9c3d4bbfc44bc918a8efd1d553e2337b11
Leonardo Felini <leonardofelini9857@gmail.com>
2019-10-19 22:13:52 -0300

    Initial Commit

---
+++ Financial Control/StockControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Financial_Control
+{
+    public partial class StockControl : Form
+    {
+        public StockControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
